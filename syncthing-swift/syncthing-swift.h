96a0233544e57425da981bfa1d7ea48377764d22
dapperstout <sir@dapperstout.com>
2014-08-20 21:12:47 +0200

    Added lz4 library.

--- syncthing-swift/syncthing-swift.h
+++ syncthing-swift/syncthing-swift.h
@@ -1,19 +1,6 @@
-//
-//  syncthing-swift.h
-//  syncthing-swift
-//
-//  Created by Mark Spanbroek on 20/08/14.
-//
-//
-
 #import <UIKit/UIKit.h>
 
-//! Project version number for syncthing-swift.
 FOUNDATION_EXPORT double syncthing_swiftVersionNumber;
-
-//! Project version string for syncthing-swift.
 FOUNDATION_EXPORT const unsigned char syncthing_swiftVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <syncthing_swift/PublicHeader.h>
-
-
+#import "lz4.h"
