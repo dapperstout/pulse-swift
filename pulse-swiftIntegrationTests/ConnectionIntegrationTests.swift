a38fe7980ceade340c9ded098aa0e7709a015c97
dapperstout <sir@dapperstout.com>
2014-12-29 20:33:52 +0100

    Cleanup.

--- pulse-swiftIntegrationTests/ConnectionIntegrationTests.swift
+++ pulse-swiftIntegrationTests/ConnectionIntegrationTests.swift
@@ -14,11 +14,6 @@ class ConnectionIntegrationTests: XCTestCase {
         connector = Connector(identity: exampleIdentity)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
     func testOpensConnection() {
         connect();
         wait(5);
