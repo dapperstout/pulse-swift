fe1acb9d32260326e1474f6d7efa66f1c73ef8c9
Stefan van den Oord <soord@mac.com>
2014-12-29 22:17:40 +0100

    Synching requires HOME

--- pulse-swiftIntegrationTests/pre-test.sh
+++ pulse-swiftIntegrationTests/pre-test.sh
@@ -9,6 +9,11 @@ if ( ! test -d /tmp/pulse-swift ); then
     )
 fi
 
+if ( test -z "$HOME" ); then
+	echo "No HOME; setting HOME to /tmp/pulse-swift/sthome"
+	export HOME=/tmp/pulse-swift/sthome
+fi
+
 # Start syncthing if it is not already running
 if ( ! curl -s -H "X-API-Key:jobt7d8klok5l8n818q1noq2t683nu" -X POST http://127.0.0.1:59296/rest/ping > /dev/null ); then
     /tmp/pulse-swift/src/github.com/syncthing/syncthing/bin/syncthing -home=/tmp/pulse-swift/sthome -no-browser -gui-apikey="jobt7d8klok5l8n818q1noq2t683nu" -gui-address="http://127.0.0.1:59296" &
