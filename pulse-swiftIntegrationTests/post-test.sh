3780a321289132e349ba19b071515fcc7f1561a5
Stefan van den Oord <soord@mac.com>
2014-12-29 19:28:01 +0100

    Fixed behaviour that syncthing keeps running after testing when it was already running before

--- pulse-swiftIntegrationTests/post-test.sh
+++ pulse-swiftIntegrationTests/post-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if ( test -f /tmp/pulse-swift/started-by-build-script ); then
+if ( test -z "$BUILD_ROOT" || test -f /tmp/pulse-swift/started-by-build-script ); then
     curl -s -H "X-API-Key:jobt7d8klok5l8n818q1noq2t683nu" -X POST http://127.0.0.1:59296/rest/shutdown
     sleep 3
-    rm /tmp/pulse-swift/started-by-build-script
+    rm -f /tmp/pulse-swift/started-by-build-script
 fi
--- pulse-swiftIntegrationTests/pre-test.sh
+++ pulse-swiftIntegrationTests/pre-test.sh
@@ -12,6 +12,8 @@ fi
 # Start syncthing if it is not already running
 if ( ! curl -s -H "X-API-Key:jobt7d8klok5l8n818q1noq2t683nu" -X POST http://127.0.0.1:59296/rest/ping > /dev/null ); then
     /tmp/pulse-swift/src/github.com/syncthing/syncthing/bin/syncthing -home=/tmp/pulse-swift/sthome -no-browser -gui-apikey="jobt7d8klok5l8n818q1noq2t683nu" -gui-address="http://127.0.0.1:59296" &
-    touch /tmp/pulse-swift/started-by-build-script
+    if ( test -n "$BUILD_ROOT" ); then
+        touch /tmp/pulse-swift/started-by-build-script
+    fi
     sleep 10
 fi
